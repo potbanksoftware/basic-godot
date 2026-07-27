1a9a325f1ec76a8252f0c2faca92ab4a4c1144d1
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-07-27 08:41:50 +0100

    Don't try to disable pause menu if running main menu standalone.

--- modules/menus/main_menu.gd
+++ modules/menus/main_menu.gd
@@ -5,7 +5,9 @@ func _ready() -> void:
 	get_parent().set_process(false)
 
 	super()
-	$"../../PauseCanvas/PauseMenu".suppress_menu = true
+	var pause_menu: PauseMenu = get_node_or_null("../../PauseCanvas/PauseMenu")
+	if pause_menu:
+		pause_menu.suppress_menu = true
 
 
 func disable_menu() -> void:
